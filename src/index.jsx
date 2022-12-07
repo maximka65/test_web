e7b16519f747044990bb1942304dc65e28087a25
maximka65 <maxim652002@gmail.com>
2022-12-07 22:00:47 +0300

    12

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Router from './routes';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,8 +8,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/test_web'>
+    <HashRouter>
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
